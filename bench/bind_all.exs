b9048130d8c01a3133c310b0bcae0e408596d595
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-10-08 14:01:44 +0700

    rerun bind_all benchmark

--- bench/bind_all.exs
+++ bench/bind_all.exs
@@ -1,5 +1,5 @@
-# Commit: 74097a2ad636df6b720f37cedb09a32ff620a622
-# Date: Sat Oct  5 01:10:17 +07 2024
+# Commit: bc9590788a32763c95d01e5ab494bcb22e8fc0d4
+# Date: Tue Oct  8 14:01:09 +07 2024
 
 # Operating System: macOS
 # CPU Information: Apple M2
@@ -9,20 +9,20 @@
 # Erlang 27.0
 # JIT enabled: true
 
-# Name                        ips        average  deviation         median         99th %
-# bind_null               24.02 M       41.63 ns  ±6270.98%          42 ns          42 ns
-# bind_float(float)       23.41 M       42.71 ns  ±5966.85%          42 ns          42 ns
-# bind_integer(int)       23.26 M       43.00 ns  ±6126.14%          42 ns          42 ns
-# bind_blob(bin)          20.05 M       49.87 ns  ±5774.13%          42 ns          84 ns
-# bind_text(text)         19.81 M       50.48 ns  ±5215.76%          42 ns          84 ns
+# Name                   ips        average  deviation         median         99th %
+# bind_null          23.88 M       41.88 ns  ±5813.10%          42 ns          42 ns
+# bind_float         23.19 M       43.12 ns  ±5936.34%          42 ns          42 ns
+# bind_integer       22.92 M       43.63 ns  ±6434.12%          42 ns          42 ns
+# bind_blob          20.16 M       49.61 ns  ±5284.18%          42 ns          84 ns
+# bind_text          20.06 M       49.86 ns  ±4985.34%          42 ns          84 ns
 
 Benchee.run(
   %{
     "bind_null" => fn %{stmt: stmt} -> XQLite.bind_null(stmt, 1) end,
-    "bind_integer(int)" => fn %{stmt: stmt} -> XQLite.bind_integer(stmt, 1, 100) end,
-    "bind_float(float)" => fn %{stmt: stmt} -> XQLite.bind_float(stmt, 1, 42.5) end,
-    "bind_text(text)" => fn %{stmt: stmt} -> XQLite.bind_text(stmt, 1, "hello") end,
-    "bind_blob(bin)" => fn %{stmt: stmt} -> XQLite.bind_blob(stmt, 1, <<0, 0, 0>>) end
+    "bind_integer" => fn %{stmt: stmt} -> XQLite.bind_integer(stmt, 1, 100) end,
+    "bind_float" => fn %{stmt: stmt} -> XQLite.bind_float(stmt, 1, 42.5) end,
+    "bind_text" => fn %{stmt: stmt} -> XQLite.bind_text(stmt, 1, "hello") end,
+    "bind_blob" => fn %{stmt: stmt} -> XQLite.bind_blob(stmt, 1, <<0, 0, 0>>) end
   },
   before_scenario: fn _input ->
     db = XQLite.open(":memory:", [:readonly, :nomutex])
